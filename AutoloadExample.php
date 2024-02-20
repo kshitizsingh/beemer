ecf5d2293f11a94cf9d5f9cf05aae342f408b472
Kshitiz Singh <kshitiz.sng@gmail.com>
2024-02-20 23:13:44 +0530

    First Commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace kshitiz\beemer;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
